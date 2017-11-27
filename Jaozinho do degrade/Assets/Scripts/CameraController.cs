b018b22bf305596ff47fd79dbe8236f5b0283438
Luiz Felipe R <luizfelipers19@gmail.com>
2017-11-26 23:58:37 -0200

    Versão 1
    Versão contendo:
    Animações implementadas no Jaozinho e Bandido
    Todos os ataques básicos implementados
    Bandido é patroller, entra em estado de alerta quando vê o player, e atira
    FALTA COLOCAR PROJETIL DO REVOLVER+DANO NO PLAYER

---
+++ Jaozinho do degrade/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject cameraTarget;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = new Vector3(cameraTarget.transform.position.x, transform.position.y, transform.position.z);
+
+	}
+}
---
+++ Jaozinho do degrade/Assets/Scripts/KickScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KickScript : MonoBehaviour {
+
+
+    public int kickDamage;
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.isTrigger != true && collision.CompareTag("Enemy"))
+        {
+            collision.SendMessageUpwards("AddDamage", kickDamage);
+        }
+    }
+
+
+
+
+}
